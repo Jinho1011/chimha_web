1cf2469761b9c0937a9942deb45fa187d20381b6
jinho1011 <gino9940@gmail.com>
2022-06-05 12:46:18 +0900

    ✨  feat: add youtube, twitch, cafe screen

---
+++ pages/cafe.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Cafe: NextPage = () => {
+  return <div>Cafe</div>;
+};
+
+export default Cafe;
---
+++ pages/youtube.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Youtube: NextPage = () => {
+  return <div>Youtube</div>;
+};
+
+export default Youtube;
